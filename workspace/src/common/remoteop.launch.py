08e55b5f7dfd6333907982100254d23ceffd3db5
jason <zzhou292@wisc.edu>
2023-09-23 13:38:12 -0500

    started with remoteop launch file

---
+++ workspace/src/common/remoteop.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='rosapi',
+            executable='rosapi_node'
+        ),
+        Node(
+            package='rosbridge_server',
+            executable='rosbridge_websocket.py'
+        ),
+        Node(
+            package='usb_cam',
+            executable='usb_cam_node_exe'
+        )
+    ])
+
